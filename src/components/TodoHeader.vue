91346fa6b0837878ed64dfe72466a818c7d03e89
Changsun-1011 <67418174+Changsun-1011@users.noreply.github.com>
2020-07-13 15:17:52 +0900

    TodoHeader コンポネート実装

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>Header</div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>
