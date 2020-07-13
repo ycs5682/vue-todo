ee6b62851d65d7812f4f4b1979db387c7a3d7ebc
Changsun-1011 <67418174+Changsun-1011@users.noreply.github.com>
2020-07-13 15:45:24 +0900

    TodoInput コンポネートの保存機能実装

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>Input</div>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">add</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      newTodoItem: ''
+    };
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = '';
+    }
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
