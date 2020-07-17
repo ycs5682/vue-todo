dca2686e7066cf81b6839667be222857624823de
유창선 <ycs5682@gmail.com>
2020-07-17 16:44:55 +0900

    オブジェクト省略(ES6対応)

--- src/App.vue
+++ src/App.vue
@@ -56,10 +56,10 @@ export default {
     }
   },
   components: {
-    TodoHeader: TodoHeader,
-    TodoInput: TodoInput,
-    TodoList: TodoList,
-    TodoFooter: TodoFooter
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 };
 </script>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -37,7 +37,7 @@ export default {
     }
   },
   components: {
-    Modal: Modal
+    Modal
   }
 };
 </script>
